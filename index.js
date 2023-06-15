3bf9933a33bbc80d8b2b49092d5f115bcfa6310f
David <Dlee.business2015@gmail.com>
2023-06-15 11:30:04 -0700

    init user routes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 5050;
+
 
 app.use(express.json());
 app.use(cors());
