1a2399850c70984b2c45109ab56c5d7feba7ac3e
David <Dlee.business2015@gmail.com>
2023-06-21 14:18:10 -0700

    add client side

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Search from './pages/Search/Search';
+import './App.scss';
+
+function App() {
+  	return (
+    	<div className="App">
+      		<BrowserRouter>
+				<Routes>
+					<Route path="/search" element={<Search/>}/>
+				</Routes>
+      		</BrowserRouter>
+    	</div>
+  	);
+}
+
+export default App;
