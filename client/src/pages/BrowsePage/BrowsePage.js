2102ecd097744143da2275b9fd3d57799fe8347b
David <Dlee.business2015@gmail.com>
2023-06-27 21:00:48 -0700

    fix browse path

--- client/src/pages/BrowsePage/BrowsePage.js
+++ client/src/pages/BrowsePage/BrowsePage.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import folder from '../../assets/images/folder-g.png';
 import './BrowsePage.scss';
 
 function BrowsePage() {
